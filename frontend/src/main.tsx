3a44a1ed4e176eb3ff085a438c93eeb94e811757
Vasil Kosturski <vasilkosturski@gmail.com>
2026-03-22 07:21:09 +0200

    feat: add routing with react-router and implement RunDetail component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.css'
 import './index.css'
-import App from './App.tsx'
+import RunsTable from './App.tsx'
+import RunDetail from './RunDetail.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MantineProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RunsTable />} />
+          <Route path="/runs/:id" element={<RunDetail />} />
+        </Routes>
+      </BrowserRouter>
     </MantineProvider>
   </StrictMode>,
 )
